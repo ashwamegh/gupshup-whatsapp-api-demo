07140574d57783355aa11f2a98c2add84c57ddfa
Shashank Shekhar <shashank7200@gmail.com>
2019-11-05 22:00:56 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
